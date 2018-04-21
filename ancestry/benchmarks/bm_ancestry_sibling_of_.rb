0c77119e3e820ce84ae003d66419b9be9e571e8f
Keenan Brock <keenan@thebrocks.net>
2018-04-20 20:43:21 -0400

    relationship benchmarks

---
+++ ancestry/benchmarks/bm_ancestry_sibling_of_.rb
@@ -0,0 +1,19 @@
+require 'bundler/setup'
+require 'active_record'
+require 'ancestry'
+require_relative 'support/benchmark_ancestry'
+
+db_setup script: "bm_ancestry.rb", depth: 4, count: 6
+
+ActiveRecord::Base.establish_connection(ENV.fetch('DATABASE_URL'))
+ActiveRecord::Migration.verbose = false
+
+class User < ActiveRecord::Base ; has_ancestry ; end
+
+obj = User.last
+root = User.roots.first
+Benchmark.ancestry("sibling_of?", time: 5) do
+  root.send(:clear_association_cache)
+  obj.send(:clear_association_cache)
+  root.sibling_of?(obj) # false
+end
