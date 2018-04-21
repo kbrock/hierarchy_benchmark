62b12da51dd50c389b66e6c67fb62a929aa525f7
Keenan Brock <keenan@thebrocks.net>
2018-04-21 19:41:55 -0400

    benchmark for scopes

---
+++ ancestry/benchmarks/bm_ancestry_id_ancestors_of.rb
@@ -0,0 +1,16 @@
+require 'bundler/setup'
+require 'active_record'
+require 'ancestry'
+require_relative 'support/benchmark_ancestry'
+
+db_setup script: "bm_ancestry.rb", depth: 4, count: 6
+
+ActiveRecord::Base.establish_connection(ENV.fetch('DATABASE_URL'))
+ActiveRecord::Migration.verbose = false
+
+class User < ActiveRecord::Base ; has_ancestry ; end
+
+obj = User.last
+Benchmark.ancestry("ancestors_of", time: 5) do
+  User.ancestors_of(obj.id).to_a
+end
---
+++ ancestry/benchmarks/bm_ancestry_id_siblings_of.rb
@@ -0,0 +1,16 @@
+require 'bundler/setup'
+require 'active_record'
+require 'ancestry'
+require_relative 'support/benchmark_ancestry'
+
+db_setup script: "bm_ancestry.rb", depth: 4, count: 6
+
+ActiveRecord::Base.establish_connection(ENV.fetch('DATABASE_URL'))
+ActiveRecord::Migration.verbose = false
+
+class User < ActiveRecord::Base ; has_ancestry ; end
+
+obj = User.roots.first.children.first
+Benchmark.ancestry("siblings_of", time: 5) do
+  User.siblings_of(obj.id).to_a
+end
